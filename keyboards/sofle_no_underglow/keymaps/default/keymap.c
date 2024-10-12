8a4e0bc503ee8307837696f824e893c6dab338dd
MagneticNeedle <vibhakar.solanki@magnifi.ai>
2024-10-12 17:56:54 +0530

    feat: enable logo

--- keyboards/sofle_no_underglow/keymaps/default/keymap.c
+++ keyboards/sofle_no_underglow/keymaps/default/keymap.c
@@ -126,7 +126,16 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   // If console is enabled, it will print the matrix position and status of each key pressed
     #ifdef CONSOLE_ENABLE
-        uprintf("KL: kc: 0x%04X, row: %2u, col: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+        uprintf(
+            "KL: kc: 0x%04X, row: %2u, col: %2u, pressed: %u, time: %5u, int: %u, count: %u\n",
+            keycode,
+            record->event.key.row,
+            record->event.key.col,
+            record->event.pressed,
+            record->event.time,
+            record->tap.interrupted,
+            record->tap.count
+        );
     #endif
     return true;
 }

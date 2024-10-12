57f7ea8a43748881feb98f659925a3204a3e4307
MagneticNeedle <vibhakar.solanki@magnifi.ai>
2024-10-12 17:56:54 +0530

    feat: enable logo

--- keyboards/sofle_no_underglow/keymaps/default/rgb.c
+++ keyboards/sofle_no_underglow/keymaps/default/rgb.c
@@ -5,8 +5,7 @@ uint8_t blue = 15;
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     for (uint8_t index = led_min; index < led_max; index++) {
-        if (index >= 40 && index < 44) rgb_matrix_set_color(index, red, green, blue);
-        if (index == 4) rgb_matrix_set_color(index, 0, 0, 0);
+        if (index == 4 || index == 41 || index == 46 || index == 51 || index == 56 ) rgb_matrix_set_color(index, red, green, blue);
     }
     return true;
 }
